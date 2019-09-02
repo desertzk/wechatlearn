81e094ddef9aa30e41f69de549d73e5769415bf1
zk <309410281@qq.com>
2019-09-02 21:06:14 +0800

    can get userinfo from wechat server

--- wechat.py
+++ wechat.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 import pdb
 
-import http.client, urllib.parse
+#import http.client, urllib.parse
 from flask import Flask, request, abort, render_template
 import hashlib
 import xmltodict
@@ -24,12 +24,12 @@ app = Flask(__name__)
 def wechat():
     """对接微信公众号服务器"""
     # 接收微信服务器发送的参数
-    pdb.set_trace()
+    #pdb.set_trace()
     signature = request.args.get("signature")
     timestamp = request.args.get("timestamp")
     nonce = request.args.get("nonce")
     print("signature"+signature)
-    pdb.set_trace()
+#    pdb.set_trace()
 
     # 校验参数
     if not all([signature, timestamp, nonce]):
@@ -108,7 +108,7 @@ def mainpage():
 
 
 # www.itcastcpp.cn/wechat8000/index
-@app.route("/wechat8000/index")
+@app.route("/wx/index")
 def index():
     """让用户通过微信访问的网页页面视图"""
     # 从微信服务器中拿去用户的资料数据
@@ -118,12 +118,14 @@ def index():
     if not code:
         return u"确实code参数"
 
+    print("code:"+code)
+    
     # 2. 向微信服务器发送http请求，获取access_token
     url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code" \
           % (WECHAT_APPID, WECHAT_APPSECRET, code)
-http.client.HTTPConnection("192.168.73.21",9091)
+#http.client.HTTPConnection("192.168.73.21",9091)
 
-
+    print("in index:"+url)
     # 使用urllib2的urlopen方法发送请求
     # 如果只传网址url参数，则默认使用http的get请求方式, 返回响应对象
     response = urllib2.urlopen(url)
@@ -131,14 +133,14 @@ http.client.HTTPConnection("192.168.73.21",9091)
     # 获取响应体数据,微信返回的json数据
     json_str = response.read()
     resp_dict = json.loads(json_str)
-
+    print("json_str:"+json_str)
     # 提取access_token
     if "errcode" in resp_dict:
         return u"获取access_token失败"
 
     access_token = resp_dict.get("access_token")
     open_id = resp_dict.get("openid")  # 用户的编号
-
+    print("open_id"+open_id)
     # 3. 向微信服务器发送http请求，获取用户的资料数据
     url = "https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s&lang=zh_CN" \
           % (access_token, open_id)
@@ -148,12 +150,13 @@ http.client.HTTPConnection("192.168.73.21",9091)
     # 读取微信传回的json的响应体数据
     user_json_str = response.read()
     user_dict_data = json.loads(user_json_str)
-
+    print("userinfo:"+user_json_str)
     if "errcode" in user_dict_data:
-        return u"获取用户信息失败"
+        return u"获取用户信息失败"+user_dict_data["errmsg"]
     else:
         # 将用户的资料数据填充到页面中
-        return render_template("index.html", user=user_dict_data)
+        #return render_template("index.html", user=user_dict_data)
+        return user_json_str
 
 
 if __name__ == '__main__':
