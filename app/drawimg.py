9a04e1eb169523b190002968878669cab83e4afa
ASUS <309410281@qq.com>
2020-01-12 23:53:14 +0800

    draw line chart  done

--- app/drawimg.py
+++ app/drawimg.py
@@ -13,27 +13,60 @@ from matplotlib.figure import Figure
 # dates = ['2015-09-12','2015-09-22','2015-12-10','2015-12-20','2015-12-22']
 # PM_25 = [80, 55,100,45,56]
 # dates = [pd.to_datetime(d) for d in dates]
-#
+
 # plt.plot(dates, PM_25)
 # plt.show()
 
-def drawlinegraph1(dates,value,xaxis,yaxis):
-    try:
-        fig = Figure()
-        axis = fig.add_subplot(1, 1, 1)
-        dates = [pd.to_datetime(d) for d in dates]
-        axis.plot(dates, value)
-        # axis.xlabel(xaxis)
-        # axis.ylabel(yaxis)
-        output = io.BytesIO()
-        FigureCanvasAgg(fig).print_png(output)
 
+fig = Figure()
+# axis = fig.add_subplot(1, 1, 1)
+dates = [pd.to_datetime(d) for d in ["2019-01-01","2019-02-01","2019-03-01","2019-04-01","2019-10-01"]]
+plt.plot(dates, [67,68,69,70,79])
+# plt.savefig(fig, format='png')
+plt.xlabel("xaxis")
+plt.ylabel("yaxis")
+plt.xticks(rotation=15)
 
-        return output
-    except Exception as ex:
-        logging.info(" failed "+ex)
+plt.show()
+
 
 def drawlinegraph(dates,value,xaxis,yaxis):
+    # try:
+    dates = [pd.to_datetime(d) for d in ["2019-01-01", "2019-02-01", "2019-03-01", "2019-04-01", "2019-10-01"]]
+    plt.plot(dates, [67, 68, 69, 70, 79])
+    # plt.savefig(fig, format='png')
+    plt.xlabel("xaxis")
+    plt.ylabel("yaxis")
+    plt.xticks(rotation=15)
+    output = io.BytesIO()
+
+    plt.savefig(output, format='png')
+
+
+    return output
+    # except Exception as ex:
+    #     logging.info(" failed "+str(ex))
+
+
+def drawlinegraph2(dates,value,xaxis,yaxis):
+    # try:
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    dates = [pd.to_datetime(d) for d in dates]
+    axis.plot(dates, value)
+
+    # axis.xlabel(xaxis)
+    # axis.ylabel(yaxis)
+    # axis.xticks(rotation=15)
+    output = io.BytesIO()
+    FigureCanvasAgg(fig).print_png(output)
+
+
+    return output
+    # except Exception as ex:
+    #     logging.info(" failed "+str(ex))
+
+def drawlinegraph2(dates,value,xaxis,yaxis):
     try:
         fig = Figure()
         axis = fig.add_subplot(1, 1, 1)
--- wechat.py
+++ wechat.py
@@ -624,6 +624,6 @@ if __name__ == '__main__':
     ts = time.time()
     logging.basicConfig(filename='log/medicallog'+str(ts)+'.log',level=logging.INFO)
     try:
-        app.run(host="0.0.0.0",port=80, debug=True)
+        app.run(host="0.0.0.0",port=8000, debug=True)
     except Exception as e:
         logging.exception(e)
